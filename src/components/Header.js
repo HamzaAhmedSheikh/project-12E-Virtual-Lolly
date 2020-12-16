6297bc56f6d4c2833a3303826c8ae287c28822c5
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-17 02:09:04 +0500

    I created home page for virtual lolly.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/main.css';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Virtual Lollipop</h1>
+        <p>because we all know someone who deserves some sugar.</p>
+    </div>
+  );
+}
---
+++ src/pages/createNew.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default function CreateNew() {
+  return (
+    <div>
+       <h1> Hello CreateNew </h1> 
+    </div>  
+  )  
+
+}
